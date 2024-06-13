61ad8216dcb28b2c952dfa16d308ae179cb02353
james <jdhales2686@gmail.com>
2024-06-13 12:26:41 -0600

    added expo-camera and expo-image libraries, sorted dependencies

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,22 @@
 // This replaces `const { getDefaultConfig } = require('expo/metro-config');`
 const { getSentryExpoConfig } = require("@sentry/react-native/metro");
+const { getDefaultConfig } = require("expo/metro-config");
 
 // This replaces `const config = getDefaultConfig(__dirname);`
 const config = getSentryExpoConfig(__dirname);
 
 module.exports = config;
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+defaultConfig.resolver.extraNodeModules = {
+  "@components": `${__dirname}/src/components`,
+  "@requests": `${__dirname}/src/requests`,
+  "@static": `${__dirname}/src/static`,
+  "@store": `${__dirname}/src/store`,
+  "@utils": `${__dirname}/src/utils`,
+  "@typeDefs": `${__dirname}/src/types`,
+  "@compStyles": `${__dirname}/src/styles/components`,
+};
+
+module.exports = defaultConfig;
